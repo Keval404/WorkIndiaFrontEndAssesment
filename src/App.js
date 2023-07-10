18c905ef255498b46c63349982770aabb1073ce8
Keval404 <keval.strange@gmail.com>
2023-07-10 09:58:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './MyCss.css'; // Import the CSS file for styling
 
-function App() {
+const MyComponent = () => {
+  const [candidates, setCandidates] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://run.mocky.io/v3/ae511409-8c0e-40ed-9336-aebcb602823d')
+      .then(response => response.json())
+      .then(data => {
+        setCandidates(data.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  const appliedCandidates = candidates.filter(candidate => candidate.status === 'Applied');
+  const rejectedCandidates = candidates.filter(candidate => candidate.status === 'Rejected');
+  const acceptedCandidates = candidates.filter(candidate => candidate.status === 'Accepted');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="section">
+        <h2 className='title'>Applied</h2>
+        {appliedCandidates.map(candidate => (
+          <div key={candidate.id} className="card">
+            <h3>{candidate.name}</h3>
+            <p className='innerText'>
+              <img src="https://via.placeholder.com/18x18" alt="Location Icon" />
+              {candidate.last_updated_at}
+            </p>
+            <p className='innerText'>
+              <img src="https://via.placeholder.com/18x18" alt="Location Icon" />
+              {candidate.location}
+            </p>
+            <p className='innerText'>
+              <img src="https://via.placeholder.com/18x18" alt="Location Icon" />
+              {candidate.gender}
+            </p>
+          </div>
+        ))}
+      </div>
+        
+      <div className="section">
+        <h2 className='title'>Accepted</h2>
+        {acceptedCandidates.map(candidate => (
+          <div key={candidate.id} className="card">
+            <h3>{candidate.name}</h3>
+            <p className='innerText'>
+              <img src="https://via.placeholder.com/18x18" alt="Location Icon" />
+              {candidate.last_updated_at}
+            </p>
+            <p className='innerText'>
+              <img src="https://via.placeholder.com/18x18" alt="Location Icon" />
+              {candidate.location}
+            </p>
+            <p className='innerText'>
+              <img src="https://via.placeholder.com/18x18" alt="Location Icon" />
+              {candidate.gender}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      <div className="section">
+        <h2 className='title'>Rejected</h2>
+        {rejectedCandidates.map(candidate => (
+          <div key={candidate.id} className="card">
+            <h3>{candidate.name}</h3>
+            <p className='innerText'>
+              <img src="https://via.placeholder.com/18x18" alt="Location Icon" />
+              {candidate.last_updated_at}
+            </p>
+            <p className='innerText'>
+              <img src="https://via.placeholder.com/18x18" alt="Location Icon" />
+              {candidate.location}
+            </p>
+            <p className='innerText'>
+              <img src="https://via.placeholder.com/18x18" alt="Location Icon" />
+              {candidate.gender}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      
     </div>
   );
-}
+};
 
-export default App;
+export default MyComponent;
